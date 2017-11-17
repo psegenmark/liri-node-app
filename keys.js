7358cd04ddf2083474cc370af2278668d88663eb
psegenmark <psegenmark@gmail.com>
2017-11-16 17:48:45 -0800

    start

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'N8Y3I958JIqgsIRDBSKZe2Rtc',
+  consumer_secret: 'HKssDCO78I4tvGpaJ5tIyZKBqhJUzWMefLRkP1fIBZiiEVCt36',
+  access_token_key: '931324642496622592-Kayi5UE9L206bC0Uzj3FzBu1VJcngYx',
+  access_token_secret: '	BqUOeOCwm5YCVA5z3qssJo1KkszOT4DjsgmkUw5LTGSVf',
+}
+
+module.exports = twitterKeys;
